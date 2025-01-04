3f3b40c7a3812d03265a22a8d40f5721b05d4006
Tramshy <simon.hansen1000@gmail.com>
2025-01-05 00:20:06 +0100

    Added TransformDistanceCompare

---
+++ Runtime/Behavior Graph/TransformDistanceCompare.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BehaviorGraph
+{
+    public class TransformDistanceCompare : NodeTransitionObject, IBaseBehaviorElement, IAllowMultiTransitionElement
+    {
+        public DataField<Transform> A;
+        public DataField<Transform> B;
+
+        public DataField<float> TargetDistance;
+
+        private enum CompareOptions { GreaterThan, LessThan }
+        [SerializeField] private CompareOptions _compareOption;
+
+        public override bool Condition()
+        {
+            float distance = Vector3.Distance(A.Value.position, B.Value.position);
+
+            if (_compareOption == CompareOptions.GreaterThan)
+                return distance >= TargetDistance.Value;
+            else
+            {
+                return distance <= TargetDistance.Value;
+            }
+        }
+    }
+}
