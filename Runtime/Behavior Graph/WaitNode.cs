284c4a3e73b182aae64a858f5095c45746d6cb1c
Tramshy <simon.hansen1000@gmail.com>
2025-01-16 15:16:54 +0100

    Removed dependency

--- Runtime/Behavior Graph/WaitNode.cs
+++ Runtime/Behavior Graph/WaitNode.cs
@@ -1,32 +1,41 @@
+using System;
 using UnityEngine;
-using TrashyTools;
 
 namespace BehaviorGraph
 {
     public class WaitNode : Node, IBaseBehaviorElement
     {
-        public DataField<float> Time;
-        [HideInInspector] public  Timer Timer;
+        public DataField<float> TimeToWait;
+
+        public DataField<bool> ShouldRestartOnExit;
+
+        [NonSerialized] private float _passedTime;
 
         public override void OnNodeStart()
-        {
-            CurrentStatus = Statuses.Running;
-
-            Timer = Timer.CreateTimer(() =>
-            {
-                CurrentStatus = Statuses.Success;
-                ResetNode();
-            }, Time.Value, false);
-        }
-
-        public override void OnNodeUpdate()
         {
             // No-op
         }
 
+        public override void OnNodeUpdate()
+        {
+            _passedTime += Time.deltaTime;
+
+            if (_passedTime >= TimeToWait.Value)
+            {
+                CurrentStatus = Statuses.Success;
+
+                _passedTime = 0;
+
+                return;
+            }
+        }
+
         public override void ResetNode()
         {
-            Timer.CancelTimer(ref Timer);
+            CurrentStatus = Statuses.Running;
+
+            if (ShouldRestartOnExit.Value)
+                _passedTime = 0;
         }
     }
 }
