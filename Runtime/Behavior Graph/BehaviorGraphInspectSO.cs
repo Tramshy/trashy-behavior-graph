aaf5857e4de7f46a6f541ae0b51019f9d09759e3
Tramshy <simon.hansen1000@gmail.com>
2025-05-09 21:15:26 +0200

    Quick optimization

--- Runtime/Behavior Graph/BehaviorGraphInspectSO.cs
+++ Runtime/Behavior Graph/BehaviorGraphInspectSO.cs
@@ -32,25 +32,36 @@ namespace BehaviorGraph
             if (!(objectHoldingPanel.GetComponent(componentType) is BehaviorData component))
                 throw new Exception($"No BehaviorData component found on: {objectHoldingPanel.name}");
 
-            foreach (FieldBindingData data in FieldOverrides)
+            for (int i = 0; i < FieldOverrides.Count; i++)
             {
-                var nodeField = GetType().GetField(data.NodeFieldName, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
-                var componentField = componentType.GetField(data.ComponentFieldOverrideName, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+                var nodeField = GetType().GetField(FieldOverrides[i].NodeFieldName, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+                var componentField = componentType.GetField(FieldOverrides[i].ComponentFieldOverrideName, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
 
-                try
-                {
-                    var value = componentField.GetValue(component);
-                    var newData = value;
-                    nodeField.SetValue(this, newData);
-                    componentField.SetValue(component, newData);
-                }
-                catch
-                {
-                    Debug.LogError($"Value not able to be associated between: {nodeField} and {componentField}, for game object: {objectHoldingPanel.name}\nNode/Transition: {this.name}");
-
-                    continue;
-                }
+                var value = componentField.GetValue(component);
+                var newData = value;
+                nodeField.SetValue(this, newData);
+                componentField.SetValue(component, newData);
             }
+            
+            //foreach (FieldBindingData data in FieldOverrides)
+            //{
+            //    var nodeField = GetType().GetField(data.NodeFieldName, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+            //    var componentField = componentType.GetField(data.ComponentFieldOverrideName, BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+
+            //    try
+            //    {
+            //        var value = componentField.GetValue(component);
+            //        var newData = value;
+            //        nodeField.SetValue(this, newData);
+            //        componentField.SetValue(component, newData);
+            //    }
+            //    catch
+            //    {
+            //        Debug.LogError($"Value not able to be associated between: {nodeField} and {componentField}, for game object: {objectHoldingPanel.name}\nNode/Transition: {this.name}");
+
+            //        continue;
+            //    }
+            //}
         }
 
         [Serializable]
